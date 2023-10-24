6af70d2ec886cd6fe23abfe2aad517a90317e16b
Nigel Gary <nigelgary97@gmail.com>
2023-10-24 14:31:52 -0400

    Made necessarry changes

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,23 @@ document.addEventListener("DOMContentLoaded", function () {
   // Toggle function for displaying a letter's result
   window.toggle = function (letter) {
     // Hide all results first
-    const allResults = document.querySelectorAll(".result");
-    allResults.forEach((result) => {
-      result.style.display = "none";
+    const allResults = document.querySelectorAll(".hidden-content");
+    allResults.forEach((content) => {
+      content.style.display = "none";
     });
 
     // Now display the clicked letter's result
-    const resultElement = document.getElementById(`${letter}-result`);
+    const resultElement = document
+      .getElementById(`${letter}-result`)
+      .querySelector(".hidden-content");
     resultElement.style.display = "block";
   };
 
   // Hide function for hiding a letter's result
   window.hide = function (letter) {
-    const resultElement = document.getElementById(`${letter}-result`);
+    const resultElement = document
+      .getElementById(`${letter}-result`)
+      .querySelector(".hidden-content");
     resultElement.style.display = "none";
   };
 });
