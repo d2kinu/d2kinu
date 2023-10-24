80c0ca4f91ecccc1bdf243399feb7cb2c4205308
Nigel Gary <nigelgary97@gmail.com>
2023-10-24 13:38:43 -0400

    fixed the hide feature

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const buttons = document.querySelectorAll(".button");
-  buttons.forEach((button) => {
-    button.addEventListener("click", function () {
-      // Hide all results first
-      const allResults = document.querySelectorAll(".result");
-      allResults.forEach((result) => {
-        result.style.display = "none";
-      });
-
-      // Now display the clicked letter's result
-      const letter = this.dataset.letter;
-      const resultElement = document.getElementById(`${letter}-result`);
-      resultElement.style.display = "block";
+  // Toggle function for displaying a letter's result
+  window.toggle = function (letter) {
+    // Hide all results first
+    const allResults = document.querySelectorAll(".result");
+    allResults.forEach((result) => {
+      result.style.display = "none";
     });
-  });
 
-  const hideButtons = document.querySelectorAll(".hide");
-  hideButtons.forEach((hideButton) => {
-    hideButton.addEventListener("click", function (event) {
-      event.stopPropagation(); // Prevent the click from reaching the .button event
-      const letter = this.dataset.letter;
-      const resultElement = document.getElementById(`${letter}-result`);
-      resultElement.style.display = "none";
-    });
-  });
+    // Now display the clicked letter's result
+    const resultElement = document.getElementById(`${letter}-result`);
+    resultElement.style.display = "block";
+  };
+
+  // Hide function for hiding a letter's result
+  window.hide = function (letter) {
+    const resultElement = document.getElementById(`${letter}-result`);
+    resultElement.style.display = "none";
+  };
 });
